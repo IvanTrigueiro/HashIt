74338889c37d94e86fa48e901e647ba90b699c7d
IvanTrigueiro <ivantrigueiro-@hotmail.com>
2018-05-25 09:31:16 -0300

    ED-10

---
+++ main.cpp
@@ -0,0 +1,155 @@
+#include <iostream>
+#include <string>
+
+#define SIZE 101
+
+using namespace std;
+
+class Hash {
+    public:
+        string table[SIZE];
+        int size;
+
+        Hash() {
+            for(int i = 0; i < SIZE; i++)
+                this->table[i] = "Empty";
+
+            this->size = 0;
+        }
+
+        bool empty() {
+            return !this->size;
+        }
+
+        bool full() {
+            return this->size >= SIZE;
+        }
+
+        int hashK(string key) {
+            int pos = 0;
+
+            for(int i = 0; i < key.size(); i++)
+                pos += key[i] * (i+1);
+
+            pos *= 19;
+
+            return pos % SIZE;
+        }
+
+        void print() {
+            cout << endl << this->size;
+
+            for(int i = 0; i < SIZE; i++)
+                if((this->table[i]).compare("Empty")) {
+                    cout << endl << i << ":" << this->table[i];
+                }
+        }
+
+        void clear() {
+            if(this->empty())
+                return;
+
+            for(int i = 0; i < SIZE; i++)
+                this->table[i] = "Empty";
+
+            this->size = 0;
+        }
+
+        int find(string name) {
+            if(this->empty())
+                return -1;
+
+            int pos = hashK(name);
+            int value = pos;
+
+            for(int i = 1, j = 0 ; j < 20; j++) {
+                if(!name.compare(this->table[pos]))
+                    return pos;
+
+                if(i < 20) {
+                    pos = ((value + (i*i) + 23 * i) % SIZE);
+                    i++;
+                }
+            }
+
+            return -1;
+        }
+
+        int insert(string name) {
+            if(this->full() || this->find(name) > -1)
+                return 0;
+
+            int pos = hashK(name);
+            int value = pos;
+
+            for(int i = 1, j = 0 ; j < 20; j++) {
+                if(!(this->table[pos]).compare("Empty")) {
+                    this->table[pos] = name;
+                    this->size++;
+                    return 1;
+                }
+
+                if(i < 20) {
+                    pos = ((value + (i*i) + 23 * i) % SIZE);
+                    i++;
+                }
+            }
+
+            return 0;
+        }
+
+        int del(string name) {
+            if(this->empty())
+                return 0;
+
+            int pos = this->find(name);
+
+            if(pos < 0)
+                return 0;
+
+            this->table[pos] = "Empty";
+            this->size--;
+            return 1;
+        }
+};
+
+int main() {
+    Hash hash;
+    int n, t;
+    string op, aux;
+
+   cin >> n;
+   cin.ignore();
+
+    do {
+        cin >> t;
+        cin.ignore();
+
+        if(t)
+            do {
+                cin >> op;
+                cin.ignore();
+
+                aux  = op.substr(4);
+
+                switch(op[0]) {
+                    case 'A':
+                        if(aux.compare(""))
+                            hash.insert(aux);
+                        break;
+
+                    case 'D':
+                        if(aux.compare(""))
+                            hash.del(aux);
+                        break;
+                }
+
+                aux = "";
+
+            }while(--t);
+
+        hash.print();
+        hash.clear();
+
+    }while(--n);
+}
